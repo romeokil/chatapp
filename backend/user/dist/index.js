a6959ecd41a52d6f37e3257ad485ad9f41e79116
romeokil <rahulkumarjha58978@gmail.com>
2026-03-11 19:06:46 +0530

    Initial commit

---
+++ backend/user/dist/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+const app = express();
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+});
+//# sourceMappingURL=index.js.map
