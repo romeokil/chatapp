c81e916b53d12982c9b726dc918f444458fa89fe
romeokil <rahulkumarjha58978@gmail.com>
2026-04-05 00:04:11 +0530

    set up redis via upstash because it gives more storage then redis.

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from 'dotenv'
 import connectDb from "./config/db.js";
+import { createClient } from "redis";
+import UserRoutes from './routes/User.js'
 dotenv.config();
 
 connectDb();
-const app=express();
 
+export const Redisclient=createClient({
+    url: process.env.REDIS_URL,
+})
+
+Redisclient.connect().then(()=>console.log("Connected to redis")).catch((error)=>console.log(error));
+
+console.log(process.env.REDIS_URL);
+const app=express();
+app.use('/api/v1',UserRoutes);
 const PORT=process.env.PORT;
 
 
