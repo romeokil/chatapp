8b51341dd540ef8a114c7866943dc18fc58d7a19
romeokil <rahulkumarjha58978@gmail.com>
2026-05-03 17:41:45 +0530

    rabbitmq consumer is mail->consumer.ts

--- backend/mail/src/index.ts
+++ backend/mail/src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import { startsendOtpConsumer } from './consumer.js';
 dotenv.config();
+
+startsendOtpConsumer();
 const app= express();
 
 const PORT=process.env.PORT ;
 app.listen(PORT,()=>{
     console.log(`Mail Server is running at ${PORT}`)
 })
+
+
